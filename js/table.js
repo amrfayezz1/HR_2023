d997a233e897bd5a0f3b07b40e57018f189f489f
Omar Ayman <omar.ayman239@gmail.com>
2023-04-16 23:01:42 +0200

    css for vacations page and minor css changes

--- js/table.js
+++ js/table.js
@@ -1,48 +1,48 @@
 // import {data} from './data.js';
 // console.log(localStorage);
 const data = [];
-for(let i=0; i< localStorage.length; i++){
-    let key = `${i+1}`;
-    let temp={
-        id: localStorage.getItem("id"+key),
-        name: localStorage.getItem("name"+key),
-        address: localStorage.getItem("address"+key),
-        dob: localStorage.getItem("dob"+key),
-        email: localStorage.getItem("email"+key),
-        gender: localStorage.getItem("gender"+key),
-        marital: localStorage.getItem("marital"+key),
-        remVac: localStorage.getItem("remVac"+key),
-        appVac: localStorage.getItem("appVac"+key),
-        phone: localStorage.getItem("phone"+key),
-        salary: localStorage.getItem("salary"+key)
+for (let i = 0; i < localStorage.length; i++) {
+    let key = `${i + 1}`;
+    let temp = {
+        id: localStorage.getItem("id" + key),
+        name: localStorage.getItem("name" + key),
+        address: localStorage.getItem("address" + key),
+        dob: localStorage.getItem("dob" + key),
+        email: localStorage.getItem("email" + key),
+        gender: localStorage.getItem("gender" + key),
+        marital: localStorage.getItem("marital" + key),
+        remVac: localStorage.getItem("remVac" + key),
+        appVac: localStorage.getItem("appVac" + key),
+        phone: localStorage.getItem("phone" + key),
+        salary: localStorage.getItem("salary" + key)
     }
     data.push(temp);
 }
 const table = document.getElementById("table");
 // short data
 const header = document.createElement("tr");
-header.innerHTML="<th>ID</th>\
+header.innerHTML = "<th>ID</th>\
                 <th>Name</th>\
-                <th>remaining vacation days</th>\
-                <th>approved vacation days</th><th></th><th></th>";
+                <th>Remaining Vacation Days</th>\
+                <th>Approved Vacation Days</th><th></th><th></th>";
 table.appendChild(header);
 for (let i = 0; i < data.length; i++) {
-    if(data[i].id != null){
+    if (data[i].id != null) {
         const row = document.createElement("tr");
-        row.innerHTML="<td>"+data[i].id+"</td>\
-                        <td>"+data[i].name+"</td>\
-                        <td>"+data[i].remVac+"</td>\
-                        <td>"+data[i].appVac+"</td>\
-                        <td><a class='link' href='edit.html?id="+data[i].id+"&name="+data[i].name+"'>Edit</a></td>\
-                        <td class='add'><a class='link' href='vacForm.html?id="+data[i].id+"&name="+data[i].name+"'>Add Vacation</a></td>";
+        row.innerHTML = "<td>" + data[i].id + "</td>\
+                        <td>"+ data[i].name + "</td>\
+                        <td>"+ data[i].remVac + "</td>\
+                        <td>"+ data[i].appVac + "</td>\
+                        <td><a class='link' href='edit.html?id="+ data[i].id + "&name=" + data[i].name + "'>Edit</a></td>\
+                        <td class='add'><a class='link' href='vacForm.html?id="+ data[i].id + "&name=" + data[i].name + "'>Add Vacation</a></td>";
         table.appendChild(row);
     }
 }
 // long data
-const info=document.getElementById("btn");
-info.addEventListener("click", ()=>{
-    table.innerHTML="";
-    header.innerHTML="<th>ID</th>\
+const info = document.getElementById("btn");
+info.addEventListener("click", () => {
+    table.innerHTML = "";
+    header.innerHTML = "<th>ID</th>\
                         <th>Name</th>\
                         <th>remaining vacation days</th>\
                         <th>approved vacation days</th>\
@@ -55,21 +55,21 @@ info.addEventListener("click", ()=>{
                         <th>date of birth</th><th></th><th></th>"
     table.appendChild(header);
     for (let i = 0; i < data.length; i++) {
-        if(data[i].id != null){
+        if (data[i].id != null) {
             const row = document.createElement("tr");
-            row.innerHTML="<td>"+data[i].id+"</td>\
-                            <td>"+data[i].name+"</td>\
-                            <td>"+data[i].remVac+"</td>\
-                            <td>"+data[i].appVac+"</td>\
-                            <td>"+data[i].email+"</td>\
-                            <td>"+data[i].address+"</td>\
-                            <td>"+data[i].phone+"</td>\
-                            <td>"+data[i].gender+"</td>\
-                            <td>"+data[i].marital+"</td>\
-                            <td>"+data[i].salary+"</td>\
-                            <td>"+data[i].dob+"</td>\
-                            <td><a class='link' href='edit.html?id="+data[i].id+"&name="+data[i].name+"'>Edit</a></td>\
-                            <td class='add'><a class='link' href='vacForm.html?id="+data[i].id+"&name="+data[i].name+"'>Add Vacation</a></td>";
+            row.innerHTML = "<td>" + data[i].id + "</td>\
+                            <td>"+ data[i].name + "</td>\
+                            <td>"+ data[i].remVac + "</td>\
+                            <td>"+ data[i].appVac + "</td>\
+                            <td>"+ data[i].email + "</td>\
+                            <td>"+ data[i].address + "</td>\
+                            <td>"+ data[i].phone + "</td>\
+                            <td>"+ data[i].gender + "</td>\
+                            <td>"+ data[i].marital + "</td>\
+                            <td>"+ data[i].salary + "</td>\
+                            <td>"+ data[i].dob + "</td>\
+                            <td><a class='link' href='edit.html?id="+ data[i].id + "&name=" + data[i].name + "'>Edit</a></td>\
+                            <td class='add'><a class='link' href='vacForm.html?id="+ data[i].id + "&name=" + data[i].name + "'>Add Vacation</a></td>";
             table.appendChild(row);
         }
     }
@@ -77,20 +77,20 @@ info.addEventListener("click", ()=>{
 })
 
 const search = document.getElementById('search');
-search.addEventListener('submit', (e)=>{
+search.addEventListener('submit', (e) => {
     e.preventDefault();
     const sName = document.getElementById("sName");
-    let newData=[];
-    if(sName.value==""){
-        newData=data;
-    }else{
-        newData = data.filter(x=>{
-            if(x.name != null)
+    let newData = [];
+    if (sName.value == "") {
+        newData = data;
+    } else {
+        newData = data.filter(x => {
+            if (x.name != null)
                 return x.name.toLowerCase().includes(sName.value.toLowerCase());
         });
     }
-    table.innerHTML="";
-    header.innerHTML="<th>ID</th>\
+    table.innerHTML = "";
+    header.innerHTML = "<th>ID</th>\
                         <th>Name</th>\
                         <th>remaining vacation days</th>\
                         <th>approved vacation days</th>\
@@ -103,24 +103,24 @@ search.addEventListener('submit', (e)=>{
                         <th>date of birth</th><th></th><th></th>"
     table.appendChild(header);
     for (let i = 0; i < newData.length; i++) {
-        if(data[i].id != null){
+        if (data[i].id != null) {
             const row = document.createElement("tr");
-            row.innerHTML="<td>"+newData[i].id+"</td>\
-                            <td>"+newData[i].name+"</td>\
-                            <td>"+newData[i].remVac+"</td>\
-                            <td>"+newData[i].appVac+"</td>\
-                            <td>"+newData[i].email+"</td>\
-                            <td>"+newData[i].address+"</td>\
-                            <td>"+newData[i].phone+"</td>\
-                            <td>"+newData[i].gender+"</td>\
-                            <td>"+newData[i].marital+"</td>\
-                            <td>"+newData[i].salary+"</td>\
-                            <td>"+newData[i].dob+"</td>\
-                            <td><a class='link' href='edit.html?id="+newData[i].id+"'>Edit</a></td>\
-                            <td class='add'><a class='link' href='vacForm.html?id="+newData[i].id+"'>Add Vacation</a></td>";
+            row.innerHTML = "<td>" + newData[i].id + "</td>\
+                            <td>"+ newData[i].name + "</td>\
+                            <td>"+ newData[i].remVac + "</td>\
+                            <td>"+ newData[i].appVac + "</td>\
+                            <td>"+ newData[i].email + "</td>\
+                            <td>"+ newData[i].address + "</td>\
+                            <td>"+ newData[i].phone + "</td>\
+                            <td>"+ newData[i].gender + "</td>\
+                            <td>"+ newData[i].marital + "</td>\
+                            <td>"+ newData[i].salary + "</td>\
+                            <td>"+ newData[i].dob + "</td>\
+                            <td><a class='link' href='edit.html?id="+ newData[i].id + "'>Edit</a></td>\
+                            <td class='add'><a class='link' href='vacForm.html?id="+ newData[i].id + "'>Add Vacation</a></td>";
             table.appendChild(row);
         }
     }
     info.remove();
-    sName.value="";
+    sName.value = "";
 });
--- js/vacations.js
+++ js/vacations.js
@@ -2,18 +2,18 @@
 // import {data} from './data.js';
 
 const data = [];
-for(let i=0; i< localStorage.length; i++){
-    let key = `${i+1}`;
-    if(localStorage.getItem("id"+key)!=null && localStorage.getItem("from"+key)!=null){
-        let temp={
-            id: localStorage.getItem("id"+key),
-            name: localStorage.getItem("name"+key),
-            remVac: localStorage.getItem("remVac"+key),
-            appVac: localStorage.getItem("appVac"+key),
-            from: localStorage.getItem("from"+key),
-            to: localStorage.getItem("to"+key),
-            reason: localStorage.getItem("reason"+key),
-            status: localStorage.getItem("status"+key)
+for (let i = 0; i < localStorage.length; i++) {
+    let key = `${i + 1}`;
+    if (localStorage.getItem("id" + key) != null && localStorage.getItem("from" + key) != null) {
+        let temp = {
+            id: localStorage.getItem("id" + key),
+            name: localStorage.getItem("name" + key),
+            remVac: localStorage.getItem("remVac" + key),
+            appVac: localStorage.getItem("appVac" + key),
+            from: localStorage.getItem("from" + key),
+            to: localStorage.getItem("to" + key),
+            reason: localStorage.getItem("reason" + key),
+            status: localStorage.getItem("status" + key)
         }
         data.push(temp);
     }
@@ -21,12 +21,12 @@ for(let i=0; i< localStorage.length; i++){
 
 const table = document.getElementById("table");
 const header = document.createElement("tr");
-header.innerHTML="<th>ID</th>\
+header.innerHTML = "<th>ID</th>\
                 <th>Name</th>\
-                <th>remaining vacation days</th>\
-                <th>approved vacation days</th>\
-                <th>from</th>\
-                <th>to</th>\
+                <th>Remaining Vacation Days</th>\
+                <th>Approved Vacation Days</th>\
+                <th>From</th>\
+                <th>To</th>\
                 <th>Reason</th>\
                 <th>Status</th>";
 table.appendChild(header);
@@ -34,56 +34,56 @@ for (let i = 0; i < data.length; i++) {
     // const selectedObject = data.find(obj => obj.id == forms[i].id);
     // console.log(selectedObject);
     const row = document.createElement("tr");
-    row.classList.add("rw"+i);
-    row.innerHTML="<td>"+data[i].id+"</td>\
-                    <td>"+data[i].name+"</td>\
-                    <td class='rem'>"+data[i].remVac+"</td>\
-                    <td class='app'>"+data[i].appVac+"</td>\
-                    <td>"+data[i].from+"</td>\
-                    <td>"+data[i].to+"</td>\
-                    <td>"+data[i].reason+"</td>\
-                    <td class='status'>"+data[i].status+"</td>\
+    row.classList.add("rw" + i);
+    row.innerHTML = "<td>" + data[i].id + "</td>\
+                    <td>"+ data[i].name + "</td>\
+                    <td class='rem'>"+ data[i].remVac + "</td>\
+                    <td class='app'>"+ data[i].appVac + "</td>\
+                    <td>"+ data[i].from + "</td>\
+                    <td>"+ data[i].to + "</td>\
+                    <td>"+ data[i].reason + "</td>\
+                    <td class='status'>"+ data[i].status + "</td>\
                     <td><button class='accept'>Approve</button></td>\
                     <td><button class='reject'>Decline</button></td></td>";
     table.appendChild(row);
-    const acc = document.querySelector(".rw"+i+" .accept");
-    const rej = document.querySelector(".rw"+i+" .reject");
-    acc.addEventListener("click", ()=>{
+    const acc = document.querySelector(".rw" + i + " .accept");
+    const rej = document.querySelector(".rw" + i + " .reject");
+    acc.addEventListener("click", () => {
         const date1 = new Date(data[i].from);
         const date2 = new Date(data[i].to);
         const timeDiff = date2.getTime() - date1.getTime();
         const dayDiff = timeDiff / (1000 * 3600 * 24);
         document.getElementsByClassName("rem")[i].innerHTML -= dayDiff;
-        document.getElementsByClassName("app")[i].innerHTML = Number(document.getElementsByClassName("app")[i].innerHTML)+ dayDiff;
+        document.getElementsByClassName("app")[i].innerHTML = Number(document.getElementsByClassName("app")[i].innerHTML) + dayDiff;
         document.getElementsByClassName("status")[i].innerHTML = "Approved";
         // selectedObject.remVac-=dayDiff;
         // selectedObject.appVac=Number(selectedObject.appVac) + dayDiff;
         // forms.splice(i,1);
-        acc.disabled=true;
-        rej.disabled=true;
-        localStorage.removeItem("from"+`${data[i].id}`);
-        localStorage.removeItem("to"+`${data[i].id}`);
-        localStorage.removeItem("reason"+`${data[i].id}`);
-        localStorage.removeItem("status"+`${data[i].id}`);
-        let rem = localStorage.getItem("remVac"+`${data[i].id}`);
-        localStorage.setItem("remVac"+`${data[i].id}`, rem-dayDiff);
-        let app = Number(localStorage.getItem("appVac"+`${data[i].id}`))+dayDiff;
-        localStorage.setItem("appVac"+`${data[i].id}`, app);
+        acc.disabled = true;
+        rej.disabled = true;
+        localStorage.removeItem("from" + `${data[i].id}`);
+        localStorage.removeItem("to" + `${data[i].id}`);
+        localStorage.removeItem("reason" + `${data[i].id}`);
+        localStorage.removeItem("status" + `${data[i].id}`);
+        let rem = localStorage.getItem("remVac" + `${data[i].id}`);
+        localStorage.setItem("remVac" + `${data[i].id}`, rem - dayDiff);
+        let app = Number(localStorage.getItem("appVac" + `${data[i].id}`)) + dayDiff;
+        localStorage.setItem("appVac" + `${data[i].id}`, app);
 
         window.location.reload();
         // console.log(selectedObject);
         // console.log(localStorage.getItem("remVac"+`${i+1}`));
         // console.log(localStorage.getItem("appVac"+`${i+1}`));
     })
-    rej.addEventListener("click", ()=>{
+    rej.addEventListener("click", () => {
         document.getElementsByClassName("status")[i].innerHTML = "Declined";
         // forms.splice(i,1);
-        localStorage.removeItem("from"+`${data[i].id}`);
-        localStorage.removeItem("to"+`${data[i].id}`);
-        localStorage.removeItem("reason"+`${data[i].id}`);
-        localStorage.removeItem("status"+`${data[i].id}`);
-        acc.disabled=true;
-        rej.disabled=true;
+        localStorage.removeItem("from" + `${data[i].id}`);
+        localStorage.removeItem("to" + `${data[i].id}`);
+        localStorage.removeItem("reason" + `${data[i].id}`);
+        localStorage.removeItem("status" + `${data[i].id}`);
+        acc.disabled = true;
+        rej.disabled = true;
         window.location.reload();
     })
 }
